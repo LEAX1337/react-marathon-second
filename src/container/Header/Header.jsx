d615d64cae75495b654bdf03bcf84c638ffe8848
NeverMasya <inf3rnalcr0w@gmail.com>
2023-07-27 17:45:40 +0300

    Keep going

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react"
 
-import './Header.css';
+import "./Header.scss"
+import { images } from "../../constants"
+import { SubHeading } from "../../components"
 
 const Header = () => (
-  <div>
-    Header
-  </div>
-);
+	<div className="app__header app__wrapper section__padding" id="home">
+		<div className="app__wrapper_info">
+			<SubHeading title="Chase the new flavour" />
+			<h1 className="app__header-h1">The Key to Fine Dining</h1>
+			<p className="p__opensans" style={{ margin: "2rem 0" }}>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus rem quis harum assumenda soluta fugit laborum maiores repellat omnis
+				molestias culpa.
+			</p>
+			<button type="button" className="custom__button bg-[--color-crimson]">Know More</button>
+		</div>
 
-export default Header;
+		<div className="app__wrapper_img">
+			<img src={images.welcome} alt="Welcome" />
+		</div>
+	</div>
+)
+
+export default Header
